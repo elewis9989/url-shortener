531c45347e60022998b639602c83aef1e0eed12d
Elijah Lewis <elijah@Elijahs-MBP.ood.io>
2022-04-03 02:42:18 -0500

    add server

--- server/config/default.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  port: 4000,
-  dbUri: 'mongodb://127.0.0.1:27017/url-shortener',
-};
--- server/config/production.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  port: 4000,
-  dbUri: 'mongodb+srv://mongo:MQNyQ6BNDHo66IH1@cluster0.rntwo.mongodb.net/test',
-};
